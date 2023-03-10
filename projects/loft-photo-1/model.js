d7670a261e7f6d51c5325605061365433db7b370
Vladimir <melnikov.v@office.lan>
2023-03-10 16:42:30 +0300

    week-2: правки по комментариям

--- projects/loft-photo-1/model.js
+++ projects/loft-photo-1/model.js
@@ -1,19 +1,23 @@
 import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
+const getRandomElement = (arr) => {
+  const randomNum = Math.floor(Math.random() * arr.length);
+
+  return arr[randomNum];
+};
+
+const getNextPhoto = () => {
+  const randomUser = getRandomElement(friendsDB);
+  const randomUserPhoto = getRandomElement(photosDB[randomUser.id]);
+
+  return {
+    friend: randomUser.firstName,
+    url: randomUserPhoto.url,
+  };
+};
+
 export default {
-  getRandomElement(array) {
-    const randomNum = Math.floor(Math.random() * array.length);
-
-    return array[randomNum];
-  },
-  getNextPhoto() {
-    const randomUser = this.getRandomElement(friendsDB);
-    const randomUserPhoto = this.getRandomElement(photosDB[randomUser.id]);
-
-    return {
-      friend: randomUser.firstName,
-      url: randomUserPhoto.url,
-    };
-  },
+  getRandomElement,
+  getNextPhoto,
 };
