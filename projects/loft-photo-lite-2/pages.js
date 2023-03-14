77c31be221245902582f4c927bb11d9e2c93bec6
Vladimir <melnikov.v@office.lan>
2023-03-14 22:21:59 +0300

    week-3: loft-photo-lite-2 (part 1)

--- projects/loft-photo-lite-2/pages.js
+++ projects/loft-photo-lite-2/pages.js
@@ -4,8 +4,28 @@ const pagesMap = {
   profile: '.page-profile',
 };
 
-console.log(pagesMap)
+const openPage = (showPage) => {
+  // Удаляем точку
+  const showClass = pagesMap[showPage].slice(1);
+
+  pages.forEach(element => {
+    if (element.classList.contains(showClass)) {
+      element.classList.remove('hidden');
+    } else if (!element.classList.contains('hidden')) {
+      element.classList.add('hidden');
+    }
+  });
+};
+
+const pages = document.querySelectorAll('.page');
+
+pages.openPage = openPage;
+
+// pages.openPage('main'); // сделать видимым элемент с классом page-main
+// pages.openPage('profile'); // сделать видимым элемент с классом page-profile, а page-main скрыть
+// pages.openPage('login'); // сделать видимым элемент с классом page-login, а page-profile скрыть
+
 
 export default {
-  openPage(name) {},
+  openPage,
 };
